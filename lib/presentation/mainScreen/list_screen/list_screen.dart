c3921e5372a24ee8a7618f6a8a8e2c76cc8c87ff
Gusarov Faddey <gusarovfg@gmail.com>
2024-08-15 02:15:41 +0400

    feat: реализовано удаление задачи

--- lib/presentation/mainScreen/list_screen/list_screen.dart
+++ lib/presentation/mainScreen/list_screen/list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:pixel_border/pixel_border.dart';
 import 'package:todolist/hive_service/hive_service.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todolist/model/item_model.dart';
@@ -23,26 +24,57 @@ class _ListScreenState extends State<ListScreen> {
           padding: const EdgeInsets.all(15.0),
           child: ListView.separated(
               itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  child: TaskListTile(
-                    task: box.values.toList()[index],
-                    index: index,
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetailScreen(
-                          task: box.values.toList()[index],
-                          index: index,
+                return Dismissible(
+                  key: ValueKey(box.values.toList()[index].title),
+                  background: Container(
+                    decoration: ShapeDecoration(
+                      color: Colors.red.shade300,
+                      shape: PixelBorder.solid(
+                          color: Colors.red,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(4),
+                          ),
+                          pixelSize: 2),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'DELETE',
+                          style: TextStyle(fontFamily: 'pixel', fontSize: 20),
                         ),
-                      ),
-                    );
+                        Text('DELETE',
+                            style:
+                                TextStyle(fontFamily: 'pixel', fontSize: 20)),
+                      ],
+                    ),
+                  ),
+                  onDismissed: (_) {
+                    HiveService().deleteTask(index: index);
                   },
+                  child: GestureDetector(
+                    child: TaskListTile(
+                      task: box.values.toList()[index],
+                      index: index,
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailScreen(
+                            task: box.values.toList()[index],
+                            index: index,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 );
               },
               separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(),
+                  const SizedBox(
+                    height: 10,
+                  ),
               itemCount: box.length),
         );
       },
